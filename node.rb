eddc2624793f393b2391b96ac071caa4dfd16b38
matristle <mercerary@tutanota.com>
2022-05-01 00:00:53 +0300

    Make Node class using Comparable module

---
+++ node.rb
@@ -0,0 +1,25 @@
+class Node
+    include Comparable
+
+    attr_reader :data
+
+    def initialize(data, left_child = nil, right_child = nil)
+        @data = data
+        @left_child = left_child
+        @right_child = right_child
+    end
+
+    def <=>(other_node)
+        data <=> other_node.data
+    end
+end
+
+#------------------------------------
+
+node1 = Node.new(1)
+node2 = Node.new(6)
+node3= Node.new(0)
+node4 = Node.new(5)
+
+p node3.between?(node1, node2)
+p node4.between?(node1, node2)
